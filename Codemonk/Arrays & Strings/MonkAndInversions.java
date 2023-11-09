ef94db32c14c9eb6c6231616cc66b641a86d97dd
shubham shingane <62791205+shubh59@users.noreply.github.com>
2023-11-09 13:49:38 +0530

    Updated Code

--- Codemonk/Arrays & Strings/MonkAndInversions.java
+++ Codemonk/Arrays & Strings/MonkAndInversions.java
@@ -5,24 +5,25 @@ M[i][j] > M[p][q]
 */
 
 import java.util.*;
+
 class TestClass {
-    public static void main(String args[] ) throws Exception {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t != 0){
+        while (t != 0) {
             int n = sc.nextInt();
             int inversion = 0;
             int M[][] = new int[n][n];
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
                     M[i][j] = sc.nextInt();
                 }
             }
-            for(int i = 0; i < n; i++){
-                for(int p = 0; p < n; p++){
-                    for(int j = 0; j <= i; j++){
-                        for(int q = 0; q <= p; q++){
-                            if(M[i][p] < M[j][q]){
+            for (int i = 0; i < n; i++) {
+                for (int p = 0; p < n; p++) {
+                    for (int j = 0; j <= i; j++) {
+                        for (int q = 0; q <= p; q++) {
+                            if (M[i][p] < M[j][q]) {
                                 inversion++;
                             }
                         }
@@ -32,6 +33,7 @@ class TestClass {
             System.out.println(inversion);
             t--;
         }
+        sc.close();
 
     }
 }
